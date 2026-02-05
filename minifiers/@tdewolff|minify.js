de5ed9fd10ff9718445d396fada233ecf487c4ca
Dillon Kearns <dillon@incrementalelm.com>
2026-02-05 11:46:54 -0800

    Update dependencies (#1)
    * Update to latest dependencies.
    
    * Update readme with latest benchmark results.

--- minifiers/@tdewolff|minify.js
+++ minifiers/@tdewolff|minify.js
@@ -1,6 +1,5 @@
 // @ts-check
-// @ts-expect-error There are no types for this package.
-import * as tdewolff from "@tdewolff/minify";
+import { minify as tdewolffMinify } from "@tdewolff/minify";
 import { minify } from "../minify.js";
 
-minify((code) => tdewolff.string("application/javascript", code));
+minify((code) => tdewolffMinify("application/javascript", code));
